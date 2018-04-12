b794709f52e513d91688dcf02a2d43dd0a682ceb
bluzioo <bluz.mao@outlook.com>
2018-04-13 00:39:34 +0800

    shiro demo

--- incubator/demo-shiro/README.md
+++ incubator/demo-shiro/README.md
@@ -1,1 +1,2 @@
 
+程序引自张开涛《跟我说Shiro》，http://jinnianshilongnian.iteye.com/blog/2037222
---
+++ incubator/demo-shiro/src/main/java/demo/shiro/web/controller/IndexController.java
@@ -0,0 +1,43 @@
+package demo.shiro.web.controller;
+
+import demo.shiro.entity.Resource;
+import demo.shiro.entity.User;
+import demo.shiro.service.ResourceService;
+import demo.shiro.service.UserService;
+import demo.shiro.web.bind.annotation.CurrentUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-2-14
+ * <p>Version: 1.0
+ */
+@Controller
+public class IndexController {
+
+    @Autowired
+    private ResourceService resourceService;
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/")
+    public String index(@CurrentUser User loginUser, Model model) {
+        Set<String> permissions = userService.findPermissions(loginUser.getUsername());
+        List<Resource> menus = resourceService.findMenus(permissions);
+        model.addAttribute("menus", menus);
+        return "index";
+    }
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+
+
+}
