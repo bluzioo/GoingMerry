71890c391fe23739c1d2e74b8c1564a72bc2e9c5
bluzioo <bluz.mao@outlook.com>
2018-04-12 22:52:31 +0800

    shiro demo web part

--- incubator/demo-shiro/README.md
+++ incubator/demo-shiro/README.md
@@ -1,7 +1,1 @@
 
-
-Bluz's OKTA:
-https://dev-975323.oktapreview.com/
-
-TO DO:
-use OKTA as remote auth
---
+++ incubator/demo-shiro/src/main/java/demo/shiro/web/controller/UserController.java
@@ -0,0 +1,23 @@
+package demo.shiro.web.controller;
+
+import demo.shiro.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("userList", userService.findAll());
+        return "user/list";
+    }
+
+}
