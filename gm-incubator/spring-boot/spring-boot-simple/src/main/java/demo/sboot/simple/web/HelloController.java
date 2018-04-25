3fadf24d4415d3b4efb5ac5313a82864d0f85b21
bluzioo <bluz.mao@outlook.com>
2018-04-25 21:56:59 +0800

    spring boot quick demo

---
+++ gm-incubator/spring-boot/spring-boot-simple/src/main/java/demo/sboot/simple/web/HelloController.java
@@ -0,0 +1,28 @@
+package demo.sboot.simple.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @Value("${demo.profile}")
+    private String profile;
+
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "hello ";
+    }
+
+
+    @RequestMapping("profile")
+    @ResponseBody
+    public String profile() {
+        return "profile is " + profile;
+    }
+
+}
---
+++ gm-incubator/spring-boot/spring-boot-simple/src/test/java/demo/sboot/simple/HelloApplicationsTest.java
@@ -0,0 +1,36 @@
+package demo.sboot.simple;
+
+import demo.sboot.simple.web.HelloController;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = HelloApplication.class)
+@WebAppConfiguration
+public class HelloApplicationsTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void testHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("hello")));
+    }
+
+}
